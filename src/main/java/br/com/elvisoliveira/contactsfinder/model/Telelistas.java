2ed3672cd2a278734afcd7f6e9b54ccf66b75ed1
Elvis Oliveira <elvis.olv@gmail.com>
2015-01-04 21:39:10 -0200

    working pagination and sout printing the name, address and link of the contact

---
+++ src/main/java/br/com/elvisoliveira/contactsfinder/model/Telelistas.java
@@ -0,0 +1,108 @@
+package br.com.elvisoliveira.contactsfinder.model;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Telelistas
+{
+
+    private final FirefoxDriver driver;
+    private final DesiredCapabilities capability;
+    private final ArrayList<String> drivers = new ArrayList<>();
+
+    private final String xpErro = "//td[contains(@background,'http://img.telelistas.net/img/por_fundotopo_erro.gif')]";
+    private final String xpNext = "//img[contains(@src,'http://img.telelistas.net/img/por_rodape_prox.gif')]/parent::a";
+
+    public Telelistas()
+    {
+        capability = DesiredCapabilities.firefox();
+        capability.setCapability("platform", Platform.ANY);
+        capability.setCapability("binary", "/bin/firefox");
+
+        driver = new FirefoxDriver(capability);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    public void close()
+    {
+        driver.close();
+        driver.quit();
+    }
+
+    public void getPage(String url)
+    {
+        // @todo: set timeout in the get method, if take too long, try again.
+        // on the third failed attempt, stop the program and send an email warning.
+        driver.get(url);
+
+        boolean found = driver.findElements(By.xpath(xpErro)).isEmpty();
+
+        // detect if there was an empty results page
+        if (found)
+        {
+            // save the current driver in the global variable "drivers"
+            drivers.add(driver.getPageSource());
+
+            // detect the presence of next button
+            while (getNext(driver))
+            {
+                // if yes, click on it
+                driver.findElement(By.xpath(xpNext)).click();
+
+                // save the current loaded "driver" in the "drivers" global variable
+                drivers.add(driver.getPageSource());
+            }
+        }
+        else
+        {
+            // no results
+            System.out.println("no result");
+        }
+    }
+
+    private boolean getNext(FirefoxDriver driver)
+    {
+        boolean next = driver.findElements(By.xpath(xpNext)).isEmpty();
+        return !(next);
+    }
+
+    public void getContacts()
+    {
+        // loop the "drivers" global variable
+        for (String html : drivers)
+        {
+            // in each case, loop check if there is more than one page, if yes retrive the 
+            // current page contacts and click on next button
+            // put the retrive contacts algoritm in another method
+            // change to jsoup
+
+            Document doc = Jsoup.parse(html);
+
+            Elements contacts = doc.select("div#Content_Regs > table");
+
+            for (Element contact : contacts)
+            {
+                String name = contact.select("td.text_resultado_ib > a").text();
+                String link = contact.select("td.text_resultado_ib > a").attr("href");
+                String addr = contact.select("td.text_endereco_ib").text();
+
+                // check if the contact exist in the database
+                // if not, register it
+                
+                System.out.println(name);
+                System.out.println(link);
+                System.out.println(addr);
+                System.out.println("---");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/br/com/elvisoliveira/contactsfinder/servlets/Busca.java
@@ -0,0 +1,34 @@
+package br.com.elvisoliveira.contactsfinder.servlets;
+
+import br.com.elvisoliveira.contactsfinder.model.Telelistas;
+import java.io.IOException;
+import java.net.URLEncoder;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Busca extends HttpServlet {
+
+    private String baseUrl;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String orgm = "0";
+        String cod_localidade = "31000";
+        String atividade = "";
+        String nome = URLEncoder.encode("jesse", "ISO-8859-1");
+        String uf_busca = "mg";
+        String imagex = "0";
+        String imagey = "0";
+
+        String url = String.format("http://www.telelistas.net/templates/resultado_busca.aspx?q=&orgm=%s&cod_localidade=%s&atividade=%s&nome=%s&uf_busca=%s&image.x=%s&image.y=%s", orgm, cod_localidade, atividade, nome, uf_busca, imagex, imagey);
+
+        Telelistas lista = new Telelistas();
+        lista.getPage(url);
+        lista.getContacts();
+        lista.close();
+    }
+
+}
